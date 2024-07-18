12811120c8ffbb74d1dc1f38a4b273050c6a894d
vishu0715 <133125417+vishu0715@users.noreply.github.com>
2024-07-18 22:11:19 +0530

    added list page

---
+++ src/components/filter/filter.jsx
@@ -0,0 +1,12 @@
+import "./filter.scss";
+
+function Filter() {
+
+    return (
+        <div className="filter">
+        Filter
+        </div>
+    )
+}
+
+export default Filter
--- src/pages/listPage/listPage.jsx
+++ src/pages/listPage/listPage.jsx
@@ -1,8 +1,17 @@
+import { listData } from '../../lib/dummydata'
 import './listPage.scss'
-
+import Filter from '../../components/filter/filter';
 function ListPage() {
+    const data = listData;
     return (
-        <div className="listPage">ListPage</div>
+        <div className="listPage">
+            <div className="listContainer">
+            <div className="wrapper">
+                <Filter />
+            </div>
+            </div>
+            <div className="mapContainer">Map</div>
+        </div>
     )
 }
 
